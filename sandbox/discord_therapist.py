103ad0b7266f661b3ce0ad314e0b3802729b2986
Oleksandr Tereshchenko <toporok@gmail.com>
2023-05-16 21:56:30 +0300

    mergebots

--- sandbox/discord_therapist.py
+++ sandbox/discord_therapist.py
@@ -1,6 +1,5 @@
 # pylint: disable=wrong-import-position
 """A simple Discord bot that reverses messages."""
-import asyncio
 import os
 import sys
 from pathlib import Path
@@ -8,6 +7,8 @@ from typing import AsyncGenerator
 
 import discord
 from dotenv import load_dotenv
+from langchain.chat_models import PromptLayerChatOpenAI
+from langchain.schema import ChatMessage
 
 sys.path.append(str(Path(__file__).parents[1]))
 from mergebots import BotMerger
@@ -21,13 +22,21 @@ DISCORD_BOT_SECRET = os.environ["DISCORD_BOT_SECRET"]
 bot_merger = BotMerger()
 discord_client = discord.Client(intents=discord.Intents.default())
 
-
 # tree = app_commands.CommandTree(discord_client)
 
 
 # @tree.command(name="start", description="Reset the dialog 🔄")
 # async def start(ctx):
 #     await ctx.send(content="Hello, World!")
+
+
+chat_llm = PromptLayerChatOpenAI(
+    model_name="gpt-4",  # "gpt-3.5-turbo",
+    request_timeout=300,
+    # temperature=0,
+    # TODO user=...,
+    pl_tags=["discord_therapist"],
+)
 
 
 @discord_client.event
@@ -37,36 +46,41 @@ async def on_ready() -> None:
     print("Logged in as", discord_client.user)
 
 
-@bot_merger.register_bot("dummy_bot", "Dummy Bot", "")
-async def dummy_bot_fulfiller(
+@bot_merger.register_bot("swipy", "Swipy", "")
+async def therapist_fulfiller(
     bot: MergedBot,
     message: MergedMessage,
     history: MergedConversation,
 ) -> AsyncGenerator[MergedMessage, None]:
     """A dummy bot that reverses messages and repeats them three times."""
-    # for num in ("one", "two", "three"):
-    #     await asyncio.sleep(1)
-    #     yield InterimBotMessage(
-    #         sender=bot,
-    #         content=f"{message.content[::-1]} {num}",
-    #     )
-    # yield FinalBotMessage(
-    #     sender=bot,
-    #     content="```" + "\n".join([msg.content for msg in conversation.messages if msg.sender.is_human]) + "```",
-    # )
-    await asyncio.sleep(0.1)
+
     conversation = [*history.messages, message]
-    yield FinalBotMessage(
-        sender=bot,
-        content="```\n"
-        + "\n".join([msg.content for msg in conversation if msg.sender.is_human and msg.is_visible_to_bots])
-        + "\n```",
+
+    llm_result = await chat_llm.agenerate(
+        [
+            [
+                ChatMessage(
+                    role="user" if msg.sender.is_human else "assistant",
+                    content=msg.content,
+                )
+                for msg in conversation
+                if msg.is_visible_to_bots
+            ]
+        ],
+        # stop=[
+        #     "\n\nHUMAN:",
+        #     "\n\nASSISTANT:",
+        # ],
     )
+    responses = llm_result.generations[0][0].text.split("\n\n")
+    for response in responses:
+        yield FinalBotMessage(sender=bot, content=response)
+
     # TODO History object is modified after yield (a previous message is appended) - is it confusing ?
     #  Probably better to rethink this (freeze, make copies etc.)
     #  But don't come back to it unless you have more than one bot in your experiment !
 
 
 if __name__ == "__main__":
-    attach_discord_client(discord_client, bot_merger, "dummy_bot")
+    attach_discord_client(discord_client, bot_merger, "swipy")
     discord_client.run(DISCORD_BOT_SECRET)
