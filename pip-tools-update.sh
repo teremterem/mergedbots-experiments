a10ced1b3d8c51b6c09cc9f757d35b2d9c703b7b
Oleksandr Tereshchenko <toporok@gmail.com>
2023-05-21 11:26:50 +0300

    mergedbots

--- pip-tools-update.sh
+++ pip-tools-update.sh
@@ -2,4 +2,4 @@
 . venv/bin/activate
 pip-compile --upgrade --resolver=backtracking requirements.in
 pip-compile --upgrade --resolver=backtracking dev-requirements.in
-pip-sync requirements.txt dev-requirements.txt
+pip-sync requirements.txt dev-requirements.txt editable-requirements.txt
