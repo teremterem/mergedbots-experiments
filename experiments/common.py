e772907626084b505918c97a47e5fed34b810015
Oleksandr Tereshchenko <toporok@gmail.com>
2023-05-28 10:56:47 +0300

    bot-manager

--- experiments/common.py
+++ experiments/common.py
@@ -1,7 +1,7 @@
 """Common stuff."""
-from mergedbots import BotManager, InMemoryObjectManager
+from mergedbots import BotManager
 
 FAST_GPT_MODEL = "gpt-3.5-turbo"
 SLOW_GPT_MODEL = "gpt-3.5-turbo"  # "gpt-4"
 
-bot_manager = BotManager(object_manager=InMemoryObjectManager())
+bot_manager = BotManager()
