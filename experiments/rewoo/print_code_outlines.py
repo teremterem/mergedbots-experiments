06ed6993904c9aeedf0ec04720b248e2a62355b1
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-15 14:40:22 +0300

    rewoo

---
+++ experiments/rewoo/print_code_outlines.py
@@ -0,0 +1,21 @@
+# pylint: disable=wrong-import-position
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+sys.path.append(str(Path(__file__).parents[2]))
+
+from experiments.rewoo.generate_code_outlines import BOTMERGER_OUTLINES_PATH
+from experiments.rewoo.rewoo_utils import list_botmerger_files
+
+
+def main() -> None:
+    for file in list_botmerger_files():
+        print()
+        print((BOTMERGER_OUTLINES_PATH / f"{file}.txt").read_text(encoding="utf-8"))
+
+
+if __name__ == "__main__":
+    main()
